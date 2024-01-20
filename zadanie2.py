12453f8225a9d5125544538d9e76d0c9fe502c52
wrx75841 <97839711+wrx75841@users.noreply.github.com>
2024-01-20 12:21:11 +0000

    feat:zadanie2

---
+++ zadanie2.py
@@ -0,0 +1,18 @@
+import random
+import string
+
+def generate_password():
+    characters = string.ascii_letters + string.digits + string.punctuation
+
+    while True:
+        password = ''.join(random.choice(characters) for i in range(8))
+
+        if any(c.islower() for c in password) and \
+           any(c.isupper() for c in password) and \
+           any(c.isdigit() for c in password) and \
+           any(c in string.punctuation for c in password) and \
+           not any(word in password.lower() for word in ["słowo1", "słowo2"]):
+            return password
+
+password_result = generate_password()
+print(password_result)
